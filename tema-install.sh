2f915aaffd42bc807a4df2a9546aec4ec563746a
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-01-31 23:10:39 +0300

    Update tema-install.sh

--- tema-install.sh
+++ tema-install.sh
@@ -5,17 +5,16 @@ sudo mkdir -p ~/.themes/Dracula
 
 #### Font yükleme
 
+git clone https://github.com/murat-akpinar/dotfile.git
+git clone https://github.com/vinceliuice/Tela-icon-theme.git
 
-wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
-unzip ~/Downloads/tema/Dracula.zip -d ~/Downloads/tema
+bash Tela-icon-theme/install.sh -n dracula
 ### Dracula tema indirme
 
 git clone https://github.com/dracula/gtk.git
 unzip ~/Downloads/tema/gtk-master.zip -d ~/Downloads/tema
 
 ### Gerekli Dosyaları Taşıma
-sudo cp -r ~/Downloads/tema/Dracula/ /usr/share/icons/
-sudo cp -r ~/Downloads/tema/Dracula/ /.icons/
 
 sudo cp -r ~/Downloads/tema/gtk/* /usr/share/themes/Dracula
 sudo cp -r ~/Downloads/tema/gtk/* /usr/share/themes/Dracula
@@ -26,11 +25,11 @@ mv ~/Downloads/tema/backgrounds/* ~/.local/share/backgrounds/
 ### Temaları Geçerli yapma
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
-gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+gsettings set org.gnome.desktop.interface icon-theme "Dracula-dark"
 
 git clone https://github.com/murat-akpinar/dotfile.git
-bash ~/Download/tema/dotfile/setup.sh
-bash ~/Download/tema/dotfile/alias.sh
+bash ~/Downloads/tema/dotfile/setup.sh
+bash ~/Downloads/tema/dotfile/alias.sh
 
 
 echo "Gnome Extension list"
