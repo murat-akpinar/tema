09b08ae94c9ab514a105d07affe65e437fbcfdb9
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-04 03:31:54 +0300

    Update tema-install.sh

--- tema-install.sh
+++ tema-install.sh
@@ -19,6 +19,7 @@ sudo cp -r ~/Downloads/tema/gtk/* /usr/share/themes/Dracula
 
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+gsettings set org.gnome.desktop.interface icon-theme 'Reversal-purple-dark'
 ### Dracula Tema ###
 
 ### Wallpaper ###
